6245fe77b85d90e35a18a49d8693219cbb2636a8
Георгий Болезин <g.bolezin@zenith24.ru>
2023-06-30 10:33:53 +0700

    Second commit after 1-st review

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from core.models import PublishedModel
 from django.contrib.auth import get_user_model
+
+from core.models import PublishedModel
+
 
 User = get_user_model()
 
@@ -9,15 +11,20 @@ class Category(PublishedModel):
     title = models.CharField(
         max_length=256, verbose_name='Заголовок', null=False
     )
-    description = models.TextField(verbose_name='Описание', null=False)
+    description = models.TextField(
+        verbose_name='Описание',
+        null=False
+    )
     slug = models.SlugField(
         max_length=64,
         unique=True,
         verbose_name='Идентификатор',
         null=False,
-        help_text=("""Идентификатор страницы для URL; """
-                   """разрешены символы латиницы, цифры, """
-                   """дефис и подчёркивание.""")
+        help_text=(
+            'Идентификатор страницы для URL; '
+            'разрешены символы латиницы, цифры, '
+            'дефис и подчёркивание.'
+        )
     )
 
     class Meta:
@@ -30,7 +37,9 @@ class Category(PublishedModel):
 
 class Location(PublishedModel):
     name = models.CharField(
-        max_length=256, verbose_name='Название места', null=False
+        max_length=256,
+        verbose_name='Название места',
+        null=False
     )
 
     class Meta:
@@ -48,29 +57,35 @@ class Post(PublishedModel):
         null=False,
         blank=False
     )
-    text = models.TextField(verbose_name='Текст')
+    text = models.TextField(
+        verbose_name='Текст'
+    )
     slug = models.SlugField(
         max_length=64,
         verbose_name='Идентификатор',
         null=False,
         blank=False,
-        help_text=("""Идентификатор страницы для URL; """
-                   """разрешены символы латиницы, цифры, """
-                   """дефис и подчёркивание.""")
+        help_text=(
+            'Идентификатор страницы для URL; '
+            'разрешены символы латиницы, цифры, '
+            'дефис и подчёркивание.'
+        )
     )
 
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
         auto_now=False,
-        help_text=("""Если установить дату и время в будущем """
-                   """— можно делать отложенные публикации.""")
+        help_text=(
+            'Если установить дату и время в будущем '
+            '— можно делать отложенные публикации.'
+        )
     )
 
     author = models.ForeignKey(
         User,
         verbose_name='Автор публикации',
         on_delete=models.CASCADE,
-        related_name='user',
+        related_name='author_posts',
         null=False,
         blank=False
     )
@@ -78,7 +93,7 @@ class Post(PublishedModel):
     location = models.ForeignKey(
         Location,
         on_delete=models.SET_NULL,
-        related_name='location',
+        related_name='location_posts',
         verbose_name='Местоположение',
         null=True
     )
@@ -86,7 +101,7 @@ class Post(PublishedModel):
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
-        related_name='category',
+        related_name='category_posts',
         verbose_name='Категория',
         null=True
     )
@@ -94,6 +109,7 @@ class Post(PublishedModel):
     class Meta:
         verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
+        ordering = ('-pub_date',)
 
     def __str__(self):
         return self.title
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,28 +1,31 @@
-from django.shortcuts import render, get_object_or_404
-from blog.models import Post, Category
 import datetime
+
+from django.shortcuts import render, get_object_or_404
+
+from blog.models import Post, Category
+
+
+def get_published_posts():
+    post_list = Post.objects.filter(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lte=datetime.datetime.now()
+    ).select_related('author', 'location', 'category')
+    return post_list
 
 
 def index(request):
     template = 'blog/index.html'
-    post_list = Post.objects.select_related('category').filter(
-        is_published=True,
-        category__is_published=True,
-        pub_date__lte=datetime.datetime.now()
-    ).order_by('-pub_date')[0:5]
+    post_list = get_published_posts()[:5]
     context = {'post_list': post_list}
     return render(request, template, context)
 
 
 def post_detail(request, pk):
     post = get_object_or_404(
-        Post.objects.filter(
-            is_published=True, category__is_published=True,
-            pub_date__lte=datetime.datetime.now()
-        ),
+        get_published_posts(),
         pk=pk
     )
-
     template = 'blog/detail.html'
     context = {'post': post}
     return render(request, template, context)
@@ -31,17 +34,11 @@ def post_detail(request, pk):
 def category_posts(request, category_slug):
     template = 'blog/category.html'
     category = get_object_or_404(
-        Category.objects.filter(
-            slug=category_slug, is_published=True
-        )
+        Category,
+        slug=category_slug,
+        is_published=True
     )
-
-    post_list = Post.objects.select_related('category').filter(
-        is_published=True,
-        category__slug=category_slug,
-        pub_date__lte=datetime.datetime.now()
-    ).order_by('-pub_date')
-
+    post_list = get_published_posts().filter(category=category)
     context = {
         'category': category,
         'post_list': post_list
